09c17a8b640889d4c9069c6869d855efd1796a74
jailgreen <36865973+jailgreen@users.noreply.github.com>
2018-05-14 19:50:40 +0300

    Automate active link

---
+++ resources/js/modules/menu.js
@@ -0,0 +1,14 @@
+/* 
+ * @license    https://opensource.org/licenses/BSD-3-Clause New BSD License
+ * @copyright  (c) 2017-2018, jailgreen jukka@jahlgren.eu
+ */
+
+import $ from 'jquery';
+
+const pathname = window.location.pathname
+const template = '.navbar-nav > a[href="' + pathname + '"]'
+const active = $(template)
+
+export default function () {
+    
+}
